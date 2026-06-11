2a861191d48653e73af855b43e774bac8272134e
frnando <frpatino6@gmail.com>
2026-06-11 16:40:49 -0500

    pùsh

--- apps/mobile/android/build.gradle.kts
+++ apps/mobile/android/build.gradle.kts
@@ -19,6 +19,22 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Fix for old plugins not specifying a namespace, required by AGP 8+.
+subprojects {
+    afterEvaluate {
+        val android = extensions.findByType(com.android.build.gradle.BaseExtension::class.java)
+        if (android != null && android.namespace == null) {
+            val manifest = file("src/main/AndroidManifest.xml")
+            if (manifest.exists()) {
+                val pkg = Regex("package=\"([^\"]+)\"").find(manifest.readText())?.groupValues?.get(1)
+                if (pkg != null) {
+                    android.namespace = pkg
+                }
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
